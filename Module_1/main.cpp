f498cdb60a930b65ebc1acab857794ae7bbfbb2b
GURUNAYK NAYAK <55269641+gravityguru@users.noreply.github.com>
2022-05-01 22:03:35 +0530

    main.cpp for add,div created

---
+++ Module_1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+main(){
+    float first_no, second_no, result_add, result_div;
+
+    std::cout<<"Enter First Number\t";
+    std::cin>> first_no;
+    std::cout<<"Enter Second Number\t";
+    std::cin>>second_no; 
+
+
+    result_add = first_no + second_no;
+    result_div = first_no / second_no;
+}

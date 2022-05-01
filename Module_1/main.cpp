375bc6275f332d80bc446a4b692af9d958a33808
GURUNAYK NAYAK <55269641+gravityguru@users.noreply.github.com>
2022-05-01 22:13:14 +0530

    Methods created.

--- Module_1/main.cpp
+++ Module_1/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+float addition (float n1, float n2){return n1+n2};
+float division (float n1, float n2){return n1/n2};
+void print_result(std::string result_type, float result_value){
+    std::cout<<result_type<<"result is: \t"<result_value<<std::endl;
+}
 
 main(){
     float first_no, second_no, result_add, result_div;
@@ -11,4 +16,7 @@ main(){
 
     result_add = first_no + second_no;
     result_div = first_no / second_no;
+
+    std::cout<<"Addition is\t "<< result_add<<std::endl;
+    std::cout<<"Division is\t "<< result_div<<std::endl;
 }

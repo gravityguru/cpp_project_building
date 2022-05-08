7eb63ada82b16ef74f9a9cc58f28806394115090
“gravityguru” <gravityguru@gmail.com>
2022-05-08 16:27:41 +0530

    ./calculator executable created using cmake and make command

--- Module_2/main.cpp
+++ Module_2/main.cpp
@@ -7,11 +7,22 @@
 //g++ main.cpp addition.cpp division.cpp print_result.cpp -o calculator
 // Dummy comment
 // git init
-//git add .
-//git status
-//git commit -m ""
+// git add .
+// git status
+// git commit -m ""
+// git remote add origin weblink (Only during the start of the project)
+// git push origin master
+
+// Type this in terminal.
+// /Desktop/cpp_soft_building/cpp_project_building/Module_2/my_build_dir
+// /Desktop/cpp_soft_building/cpp_project_building/Module_2/my_build_dir$ cmake ..
+// After writing in Cmakelist.txt
+// /Desktop/cpp_soft_building/cpp_project_building/Module_2/my_build_dir$ cmake ..
+// /Desktop/cpp_soft_building/cpp_project_building/Module_2/my_build_dir$ make
+// ./calculator (executable)
 
 
+ 
 main(){
     float first_no, second_no, result_add, result_div;
 
---
+++ Module_2/my_build_dir/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/main.cpp.o"
+  "CMakeFiles/calculator.dir/addition.cpp.o"
+  "CMakeFiles/calculator.dir/division.cpp.o"
+  "CMakeFiles/calculator.dir/print_result.cpp.o"
+  "calculator.pdb"
+  "calculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
